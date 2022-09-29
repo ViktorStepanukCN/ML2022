598302cf47f4a830f62531392000428aa360b8bd
Viktor Stepaňuk <stepanuk@cngroup.dk>
2022-09-29 12:36:57 +0200

    2. cviko

---
+++ 2/module.py
@@ -0,0 +1,5 @@
+def array_multiplication(first_array, second_array):
+    result = []
+    for i in range(len(first_array)):
+        result.append(first_array[i] * second_array[i])
+    return result
